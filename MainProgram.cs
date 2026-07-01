b680f433559cdd2c413562f653ff129d3a535ed0
killumc <dshmykova399@gmail.com>
2026-07-01 12:01:28 +0400

    create

---
+++ MainProgram.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPerfomansLab
+{
+    internal class MainProgram
+    {
+        static void Main()
+        {
+            Task1.SolutionTask1();
+            Task2.SolutionTask2();
+
+        }
+    }
+}
---
+++ Task1/Task1.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Task1
+{
+    public static void SolutionTask1()
+    {
+        Console.WriteLine("Hello, Task1!");
+    }
+}
